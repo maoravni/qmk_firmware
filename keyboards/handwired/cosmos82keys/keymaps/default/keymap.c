8565cfe125aae9d9121d0a65f24be272c481596e
Maor Avni <maor.avni@gmail.com>
2025-06-28 09:30:36 +0300

    commit

--- keyboards/handwired/cosmos82keys/keymaps/default/keymap.c
+++ keyboards/handwired/cosmos82keys/keymaps/default/keymap.c
@@ -115,24 +115,24 @@ bool oled_task_user(void) {
 //     return rotation;
 // }
 
-// void oled_render_boot(bool bootloader) {
-//     oled_clear();
-//     for (int i = 0; i < 16; i++) {
-//         oled_set_cursor(0, i);
-//         if (bootloader) {
-//             oled_write_P(PSTR("Awaiting New Firmware "), false);
-//         } else {
-//             oled_write_P(PSTR("Rebooting "), false);
-//         }
-//     }
+void oled_render_boot(bool bootloader) {
+    oled_clear();
+    for (int i = 0; i < 16; i++) {
+        oled_set_cursor(0, i);
+        if (bootloader) {
+            oled_write_P(PSTR("Awaiting New Firmware "), false);
+        } else {
+            oled_write_P(PSTR("Rebooting "), false);
+        }
+    }
 
-//     oled_render_dirty(true);
-// }
+    oled_render_dirty(true);
+}
 
-// bool shutdown_user(bool jump_to_bootloader) {
-//     oled_render_boot(jump_to_bootloader);
-//     return true;
-// }
+bool shutdown_user(bool jump_to_bootloader) {
+    oled_render_boot(jump_to_bootloader);
+    return true;
+}
 
 #endif
 
